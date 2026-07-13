e2c82caf020274d0358b8e7089ea162835810c89
Pranoy Jayaraj <pranoyjayaraj@yahoo.in>
2026-07-13 20:14:03 +0530

    CI: added workflow for nightly run to cover test match functionality
    Signed-off-by: Pranoy Jayaraj <pranoyjayaraj@yahoo.in>

--- ci/scripts/run.sh
+++ ci/scripts/run.sh
@@ -1,12 +1,15 @@
 
-# accept arguments workspace name, and overs
-# usage: ./run.sh <workspace_name> <overs>
+# accept arguments workspace name, overs, and optional extra BookCricket.py
+# autoplay flags ("test", "fast")
+# usage: ./run.sh <workspace_name> <overs> [test] [fast]
 
 WORKSPACE=$1
 OVERS=$2
+shift 2
+EXTRA_ARGS="$@"
 
 cd $WORKSPACE
 python -m pip install --upgrade pip
 # virtual environment is created by default in GitHub Actions
 pip install -r requirements.txt
-python3 BookCricket.py autoplay $OVERS
+python3 BookCricket.py autoplay $OVERS $EXTRA_ARGS
